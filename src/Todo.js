456107ba6dce8b4677f2c22b08ef30f229bb66dc
Prakhar Pal <prakharpalpfdr+github.com>
2023-02-10 00:56:49 +0530

    feat: update todo if the user presses enter

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,21 @@
 import { Widget } from "./lib";
 
+/**
+ * Features of Todo component
+ * 1. Delete todo
+ * 2. Update todo
+ *  a. using update button
+ *  b. by pressing enter
+ */
+
 export function TodoV2 ({ todo, updateTodo, handleDeleteTodo, onEditToggle }) {
     const el = todo.isEdit ? Widget("input", {
-        value: todo.text
+        value: todo.text,
+        onkeydown: event => {
+            if(event.keyCode === 13){
+                updateTodo(todo.id, el.value);
+            }
+        }
     }): Widget("span", { children: todo.text });
     return Widget("li", {
         children: [
