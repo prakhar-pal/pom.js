c7d4805c3030e1eb139510d14fcee156ee901dd7
Prakhar Pal <prakharpalpfdr+github.com>
2023-02-18 01:23:50 +0530

    feat: add increment and decrement button for Counter app

--- src/Counter.js
+++ src/Counter.js
@@ -5,13 +5,34 @@ class Counter extends Component {
     constructor() {
         super();
         setInterval(() => {
-            this.setState(state => ({...state, counter: state.counter+1 }));
+            // this.setState(state => ({...state, counter: state.counter+1 }));
         }, 1000);
     }
+
+    handleCounterChange = value => this.setState(state => ({...state, counter: state.counter + value}));
+    handleIncrement = () => this.handleCounterChange(1);
+    handleDecrement = () => this.handleCounterChange(-1);
     render = () => {
         const { counter } = this.state;
-        return Widget('div', {
-            innerHTML: counter
+        return Widget("div", {
+            className: "counter-app-container",
+            children: [
+                Widget("span", { 
+                    innerHTML: counter,
+                }),
+                Widget("div", {
+                    children: [
+                        Widget("button", {
+                            innerHTML: "+",
+                            onclick: this.handleIncrement
+                        }),
+                        Widget("button", {
+                            innerHTML: "-",
+                            onclick: this.handleDecrement
+                        }),
+                    ]
+                })
+            ]
         });
     }
 }
